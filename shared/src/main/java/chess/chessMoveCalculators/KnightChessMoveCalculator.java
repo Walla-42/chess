7b57e4baf6f8286a7dc93c8744b8b2e7b8864fef
Walla-42 <loganpwallace@gmail.com>
2025-06-25 14:53:57 -0600

    Implemented Knight movement logic

--- shared/src/main/java/chess/chessMoveCalculators/KnightChessMoveCalculator.java
+++ shared/src/main/java/chess/chessMoveCalculators/KnightChessMoveCalculator.java
@@ -1,15 +1,38 @@
 package chess.chessMoveCalculators;
 
-import chess.ChessBoard;
-import chess.ChessMove;
-import chess.ChessPosition;
+import chess.*;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 
 public class KnightChessMoveCalculator implements PieceMovesCalculator{
-    @Override
+    private static final int[][] allowedMoves = {{1,2}, {2,1}, {-1,2}, {-2,1}, {-2,-1}, {-1,-2}, {2,-1}, {1,-2}};
+
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        List<ChessMove> moves = new ArrayList<>();
+        ChessPiece piece = board.getPiece(myPosition);
+        if (piece == null) return moves;
+
+        ChessGame.TeamColor team = piece.getTeamColor();
+
+        for (int[] movementDir: allowedMoves){
+            int row = myPosition.getRow() + movementDir[0];
+            int col = myPosition.getColumn() + movementDir[1];
+
+            if (board.isInBounds(row, col)){
+                ChessPosition newPiecePosition = new ChessPosition(row, col);
+                ChessPiece target = board.getPiece(newPiecePosition);
+
+                if (target == null){
+                    moves.add(new ChessMove(myPosition, newPiecePosition));
+                } else if (target.getTeamColor() != team){
+                    moves.add(new ChessMove(myPosition, newPiecePosition));
+                }
+            }
+
+        }
+        return moves;
     }
 }

697d6d89f7483d786a0e508050a096c744be4c7c
Walla-42 <loganpwallace@gmail.com>
2025-06-25 14:43:14 -0600

    Implemented Queen movement logic

--- shared/src/main/java/chess/chessMoveCalculators/QueenChessMoveCalculator.java
+++ shared/src/main/java/chess/chessMoveCalculators/QueenChessMoveCalculator.java
@@ -1,15 +1,43 @@
 package chess.chessMoveCalculators;
 
-import chess.ChessBoard;
-import chess.ChessMove;
-import chess.ChessPosition;
+import chess.*;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 
 public class QueenChessMoveCalculator implements PieceMovesCalculator{
-    @Override
-    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+    private static final int[][] allowedMoves = {{-1,1}, {1,1}, {1,-1}, {-1,-1}, {-1,0}, {1,0}, {0,-1}, {0,1}};
+
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition){
+        List<ChessMove> moves = new ArrayList<>();
+        ChessPiece piece = board.getPiece(myPosition);
+        if (piece == null) return moves;
+
+        ChessGame.TeamColor team = piece.getTeamColor();
+
+        for (int[] movementDir: allowedMoves){
+            int row = myPosition.getRow() + movementDir[0];
+            int col = myPosition.getColumn() + movementDir[1];
+
+            while (board.isInBounds(row, col)){
+                ChessPosition newPiecePosition = new ChessPosition(row, col);
+                ChessPiece target = board.getPiece(newPiecePosition);
+
+                if (target == null){
+                    moves.add(new ChessMove(myPosition, newPiecePosition));
+                } else if (target.getTeamColor() != team){
+                    moves.add(new ChessMove(myPosition, newPiecePosition));
+                    break;
+                } else {
+                    break; // target position is occupied by piece of same team color
+                }
+
+                row += movementDir[0];
+                col += movementDir[1];
+            }
+        }
+        return moves;
     }
 }

351a8284e6e3e9587a2024f3299342f86fa18005
Walla-42 <loganpwallace@gmail.com>
2025-06-30 07:40:42 -0600

    Simplified Pawn Code and made more readable

--- shared/src/main/java/chess/chessMoveCalculators/PawnChessMoveCalculator.java
+++ shared/src/main/java/chess/chessMoveCalculators/PawnChessMoveCalculator.java
@@ -7,12 +7,10 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 
-
 /**
  * Pawn Move Logic class. Used for calculating possible moves on the gameBoard. Implements PieceMovesCalculator Template.
  */
 public class PawnChessMoveCalculator implements PieceMovesCalculator {
-
     /**
      * method for calculating the moves available to a pawn on the gameBoard
      *
@@ -20,65 +18,67 @@ public class PawnChessMoveCalculator implements PieceMovesCalculator {
      * @param myPosition position of the targeted gamePiece on the gameBoard
      * @return HashSet of all possible moves called moves
      */
-    public HashSet<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        HashSet<ChessMove> moves = new HashSet<>();
+    public HashSet<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition){
 
+        // Basic movement based on color:
         ChessPiece piece = board.getPiece(myPosition);
-        if (piece == null) return moves;
-
         ChessGame.TeamColor team = piece.getTeamColor();
+
+        HashSet<ChessMove> moves = new HashSet<>();
 
         int startRow = (team == ChessGame.TeamColor.WHITE) ? 2 : 7;
         int promotionRow = (team == ChessGame.TeamColor.WHITE) ? 8 : 1;
-        int moveDirection = (team == ChessGame.TeamColor.WHITE) ? 1 : -1;
+
+        int pieceMovement = (team == ChessGame.TeamColor.WHITE) ? 1 : -1;
 
         int row = myPosition.getRow();
 
-        // forward moves
-        if (board.isInBounds(row + moveDirection, myPosition.getColumn())) {
-            ChessPosition newChessPosition = new ChessPosition(row + moveDirection, myPosition.getColumn());
+        // Forward Moves:
+        if (board.isInBounds(row + pieceMovement, myPosition.getColumn())){
+            ChessPosition newChessPosition = new ChessPosition(row + pieceMovement, myPosition.getColumn());
             ChessPiece target = board.getPiece(newChessPosition);
 
-            if (target == null) {
-                addPromotions(myPosition, newChessPosition, promotionRow, moves);
-                if (row == startRow) {
-                    newChessPosition = new ChessPosition(row + moveDirection * 2, myPosition.getColumn());
-                    target = board.getPiece(newChessPosition);
+            if (target == null){
+                chessPiecePromotion(myPosition, moves, promotionRow, newChessPosition);
 
-                    if (target == null) {
-                        moves.add(new ChessMove(myPosition, newChessPosition));
+                if (startRow == row){
+                    if (board.isInBounds(row + pieceMovement*2, myPosition.getColumn())){
+                        newChessPosition = new ChessPosition(row + pieceMovement*2, myPosition.getColumn());
+                        target = board.getPiece(newChessPosition);
+
+                        if (target == null){
+                            moves.add(new ChessMove(myPosition, newChessPosition));
+                        }
                     }
                 }
             }
-
         }
 
-        // diagonal attack moves
-        int[] attackColumns = {1, -1};
-        for (int column : attackColumns) {
-            if (board.isInBounds(row + moveDirection, myPosition.getColumn() + column)) {
-                ChessPosition newChessPosition = new ChessPosition(row + moveDirection, myPosition.getColumn() + column);
-                ChessPiece target = board.getPiece(newChessPosition);
+        // Diagonal Attack Moves:
+        int[] attackColumns = {-1,1};
+        for (int attackMove : attackColumns){
+            if (board.isInBounds(myPosition.getRow() + pieceMovement, myPosition.getColumn() + attackMove)){
+                ChessPosition newAttackPosition = new ChessPosition(row + pieceMovement, myPosition.getColumn()+attackMove);
+                ChessPiece attackTarget = board.getPiece(newAttackPosition);
+                if (attackTarget == null) continue;
 
-                if (target != null && target.getTeamColor() != team) {
-                    addPromotions(myPosition, newChessPosition, promotionRow, moves);
+                if (attackTarget.getTeamColor() != team){
+                    chessPiecePromotion(myPosition, moves, promotionRow, newAttackPosition);
+
                 }
             }
         }
         return moves;
     }
 
-    public void addPromotions(ChessPosition currPosition, ChessPosition newPosition, int promotionRow, HashSet<ChessMove> ChessMoves) {
-        if (newPosition.getRow() == promotionRow) {
-            ChessPiece.PieceType[] promotions = new ChessPiece.PieceType[]{ChessPiece.PieceType.ROOK,
-                    ChessPiece.PieceType.KNIGHT, ChessPiece.PieceType.QUEEN, ChessPiece.PieceType.BISHOP};
-
-            for (ChessPiece.PieceType chessPiece : promotions) {
-                ChessMoves.add(new ChessMove(currPosition, newPosition, chessPiece));
-            }
-
+    private void chessPiecePromotion(ChessPosition myPosition, HashSet<ChessMove> moves, int promotionRow, ChessPosition newChessPosition) {
+        if (newChessPosition.getRow() == promotionRow){
+            moves.add(new ChessMove(myPosition, newChessPosition, ChessPiece.PieceType.QUEEN));
+            moves.add(new ChessMove(myPosition, newChessPosition, ChessPiece.PieceType.ROOK));
+            moves.add(new ChessMove(myPosition, newChessPosition, ChessPiece.PieceType.BISHOP));
+            moves.add(new ChessMove(myPosition, newChessPosition, ChessPiece.PieceType.KNIGHT));
         } else {
-            ChessMoves.add(new ChessMove(currPosition, newPosition));
+            moves.add(new ChessMove(myPosition, newChessPosition));
         }
     }
 }

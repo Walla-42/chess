840595cdd5da6006cddf359eba1df8ccff3dde13
Walla-42 <loganpwallace@gmail.com>
2025-08-04 13:38:13 -0600

    implemented broadcast method of ConnectionManager.java

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -4,6 +4,7 @@ import org.eclipse.jetty.websocket.api.Session;
 import websocket.messages.ServerMessage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ConnectionManager {
@@ -18,7 +19,47 @@ public class ConnectionManager {
         connections.remove(username);
     }
 
-    public void broadcast(String excludedUser, ServerMessage message) throws IOException {
-        throw new RuntimeException("not yet implemented");
+    /**
+     * Fetches usernames of all users connected to a specific game
+     *
+     * @param gameID ID of game
+     * @return ArrayList of usernames connected to the gameID specified
+     */
+    public ArrayList<String> getConnections(int gameID) {
+        ArrayList<String> gameConnections = new ArrayList<>();
+        for (Connection conn : connections.values()) {
+            if (conn.gameID == gameID) {
+                gameConnections.add(conn.username);
+            }
+        }
+        return gameConnections;
+    }
+
+    /**
+     * broadcasts messages to all users connected to the given gameID's game
+     *
+     * @param excludeVisitorName username of root user
+     * @param gameID             game ID of current game
+     * @param message            message to be sent to all users
+     * @throws IOException
+     */
+    public void broadcast(String excludeVisitorName, int gameID, ServerMessage message) throws IOException {
+        ArrayList<String> gameConnections = getConnections(gameID);
+        var removeList = new ArrayList<Connection>();
+        for (String user : gameConnections) {
+            Connection conn = connections.get(user);
+            if (conn.session.isOpen()) {
+                if (!conn.username.equals(excludeVisitorName)) {
+                    conn.send(message.toString()); // may need to modify this to send a json string instead
+                }
+            } else {
+                removeList.add(conn);
+            }
+        }
+
+        // Clean up any connections that were left open.
+        for (var conn : removeList) {
+            connections.remove(conn.username);
+        }
     }
 }
